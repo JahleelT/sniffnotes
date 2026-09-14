286884dd7e2ca8db14a71d2ac82ca1b5d10e87f7
JahleelT <jlt9287@nyu.edu>
2026-09-13 22:55:05 -0400

    initialized Next.js and created components for Home page

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import Header from "@/components/Header";
+import HeroSection from "@/components/HeroSection";
+
+export default function Home() {
+  return (
+    <>
+      <Header/>
+
+      <main>
+        <HeroSection/>
+      </main>
+      
+    </>
+  );
+}
