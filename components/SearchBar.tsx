5bc71ea047ae0d977c0bf6eb4a5cb30f6fc3dce2
JahleelT <jlt9287@nyu.edu>
2026-09-16 22:59:23 -0400

    created phase 1 Searchbar logic and styling"

---
+++ components/SearchBar.tsx
@@ -0,0 +1,30 @@
+"use client";
+import { useState } from "react";
+
+export default function SearchBar() {
+    const [query, setQuery] = useState("");
+
+    const handleSearch = () => {
+        const trimmedQuery = query.trim();
+
+        if (!trimmedQuery) return;
+
+        console.log(trimmedQuery);
+    };
+
+
+    return (
+        <input 
+            value = {query}
+            onChange={(e) => setQuery(e.target.value)}
+            onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                    handleSearch();
+                }
+            }}
+            type="text" 
+            placeholder="What are you in the mood to sniff today?"
+            className="w-full px-6 py-4 rounded-full border" 
+        />
+    )
+}
