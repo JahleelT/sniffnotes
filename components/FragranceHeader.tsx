dca97d9093900b0d0531ca2feba8d5b1ccba4297
JahleelT <jlt9287@nyu.edu>
2026-09-20 00:50:26 -0400

    finished first draft of FragranceHeader

--- components/FragranceHeader.tsx
+++ components/FragranceHeader.tsx
@@ -1,9 +1,20 @@
 
 
+/*
+type FragranceHeaderProps = {
+    name: string,
+    brand: string,
+    collection?: string,
+};
+*/
+
 export default function FragranceHeader() {
 
 
     return (
-        <div></div>
+        <div className="py-8 w-full bg-slate-900">
+            <h1 className="text-2xl font-semibold text-cente text-white">Aswan</h1>
+            <h2 className=" mt-1 text-sm font-medium text-center text-gray-500">Widian • Sapphire Collection</h2>
+        </div>
     );
 }
