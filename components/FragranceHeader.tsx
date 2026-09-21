9146f76020f097bbac8b175862a938022224ee81
JahleelT <jlt9287@nyu.edu>
2026-09-20 22:23:29 -0400

    minor tweaks + Aswan bg

--- components/FragranceHeader.tsx
+++ components/FragranceHeader.tsx
@@ -12,9 +12,9 @@ export default function FragranceHeader() {
 
 
     return (
-        <div className="py-8 w-full bg-slate-900">
+        <div className="py-8 w-full bg-linear-to-br from-slate-950 via-blue-950 to-blue-700">
             <h1 className="text-2xl font-semibold text-cente text-white">Aswan</h1>
-            <h2 className=" mt-1 text-sm font-medium text-center text-gray-500">Widian • Sapphire Collection</h2>
+            <h2 className=" mt-1 text-sm font-medium text-center text-amber-300">Widian • Sapphire Collection</h2>
         </div>
     );
 }
--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -14,6 +14,6 @@ export default function HeroSection() {
 
                 <MoodTags/>
             </div>
-        </section>
+        </section> 
     )
 }
