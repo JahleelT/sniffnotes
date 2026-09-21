8538bc3460c8251234f10772f288e4a5090f17a5
JahleelT <jlt9287@nyu.edu>
2026-09-20 21:52:58 -0400

    placed dummy description in Description

---
+++ components/Description.tsx
@@ -0,0 +1,8 @@
+export default function Description() {
+
+    return (
+        <h3 className="italic">
+            Embark on a timeless journey through Egypt’s ancient wonders, where the crisp citrus of bergamot and the sweet allure of raspberry intertwine with rich cedarwood and delicate violet. A fragrance that captures the essence of desert winds and eternal memories, evoking the spirit of history in every drop.
+        </h3>
+    )
+}
