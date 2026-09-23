1c7111834386db0221df8d9bf2d550507d4d8530
JahleelT <jlt9287@nyu.edu>
2026-09-22 22:32:28 -0400

    added simple styling

--- components/Description.tsx
+++ components/Description.tsx
@@ -6,7 +6,7 @@ export default function Description() {
 
             <hr className="my-6 border-gray-500 mb-2 mt-2" />
 
-            <p className="flex leading-relaxed text-base text-white text-xl py-3 px-2">
+            <p className="flex leading-relaxed text-white text-xl py-3 px-2">
             Embark on a timeless journey through Egypt’s ancient wonders, where the crisp citrus of bergamot and the sweet allure of raspberry intertwine with rich cedarwood and delicate violet. A fragrance that captures the essence of desert winds and eternal memories, evoking the spirit of history in every drop.
             </p>
         </div>
